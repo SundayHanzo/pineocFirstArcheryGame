e5cb13257906e5731304d126299645c96ea99162
Allen Lee <pineoc@naver.com>
2016-10-17 17:36:09 +0900

    testing shoot the arrows

--- Assets/Arrow.cs
+++ Assets/Arrow.cs
@@ -3,13 +3,17 @@ using System.Collections;
 
 public class Arrow : MonoBehaviour {
 
+	public int moveSpeed;
+	public Vector3 direction;
+
 	// Use this for initialization
 	void Start () {
+		//moveSpeed = 0;
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		transform.Translate (direction * moveSpeed * Time.deltaTime);
 	}
 }
---
+++ Assets/ArrowFire.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArrowFire : MonoBehaviour {
+	public GameObject Quad_arrow = null;
+	public Transform ArrowLocation = null;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.touchCount > 0 || Input.GetKeyUp(KeyCode.Space) == true) {
+			print ("Arrow Fire!");
+			Instantiate (Quad_arrow, ArrowLocation.position, ArrowLocation.rotation);
+		}
+	}
+}
