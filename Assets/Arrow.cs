3f74cd55000cdc2211bd765608272f3ed236f3d2
Allen Lee <pineoc@naver.com>
2016-10-24 00:03:09 +0900

    fire arrow on screen

--- Assets/Arrow.cs
+++ Assets/Arrow.cs
@@ -4,16 +4,18 @@ using System.Collections;
 public class Arrow : MonoBehaviour {
 
 	public int moveSpeed;
-	public Vector3 direction;
+	private Vector3 direction;
 
 	// Use this for initialization
 	void Start () {
 		//moveSpeed = 0;
-	
+		//direction = Camera.main.transform.TransformDirection(Vector3.forward);
+		//direction.y = 0;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.Translate (direction * moveSpeed * Time.deltaTime);
+		//transform.Translate (direction * moveSpeed * Time.deltaTime);
 	}
 }
--- Assets/ArrowFire.cs
+++ Assets/ArrowFire.cs
@@ -7,14 +7,19 @@ public class ArrowFire : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		//Object.Destroy (Quad_arrow, 10f);
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.touchCount > 0 || Input.GetKeyUp(KeyCode.Space) == true) {
-			print ("Arrow Fire!");
-			Instantiate (Quad_arrow, ArrowLocation.position, ArrowLocation.rotation);
+		if ((Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended) || 
+			Input.GetKeyUp(KeyCode.Space) == true) {
+			Vector3 ArrowFirePos = Camera.main.ScreenToWorldPoint (new Vector3(Screen.width/2, Screen.height/2, Camera.main.nearClipPlane));
+
+			//arrow object set, addforce
+			GameObject arrowObj = Instantiate (Quad_arrow, ArrowLocation.position, ArrowLocation.rotation) as GameObject;
+			arrowObj.GetComponent<Rigidbody> ().AddForce (Camera.main.transform.forward * 500);
 		}
 	}
 }
