41e498d13c9e1b687bf4d3967f2e49fb18a3f4ef
Allen Lee <pineoc@naver.com>
2016-10-28 16:21:52 +0900

    arrow fire power control testing

--- Assets/ArrowFire.cs
+++ Assets/ArrowFire.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class ArrowFire : MonoBehaviour {
 	public GameObject Quad_arrow = null;
 	public Transform ArrowLocation = null;
+	private float powerVal = 1.0f;
 
 	// Use this for initialization
 	void Start () {
@@ -13,13 +14,21 @@ public class ArrowFire : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		if ((Input.touchCount > 0) || Input.GetKeyDown (KeyCode.Space) == true) {
+			//touch down started
+			if(powerVal < 50.0f)
+				powerVal += 0.1f;
+		}
 		if ((Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended) || 
 			Input.GetKeyUp(KeyCode.Space) == true) {
+			//touch up ended
 			Vector3 ArrowFirePos = Camera.main.ScreenToWorldPoint (new Vector3(Screen.width/2, Screen.height/2, Camera.main.nearClipPlane));
 
 			//arrow object set, addforce
 			GameObject arrowObj = Instantiate (Quad_arrow, ArrowLocation.position, ArrowLocation.rotation) as GameObject;
-			arrowObj.GetComponent<Rigidbody> ().AddForce (Camera.main.transform.forward * 500);
+			arrowObj.GetComponent<Rigidbody> ().AddForce (Camera.main.transform.forward * 500 * powerVal);
+			print ("powerVal : " + powerVal);
+			powerVal = 1.0f;
 		}
 	}
 }
