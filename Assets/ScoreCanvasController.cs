23f7c913fcb36f42b1cf9f478356c9b73a2775c8
Allen Lee <pineoc@naver.com>
2016-11-02 23:55:00 +0900

    edit some script for score board

--- Assets/ScoreCanvasController.cs
+++ Assets/ScoreCanvasController.cs
@@ -7,10 +7,13 @@ public class ScoreCanvasController : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		ScoreText = this.GetComponent<Text> ();
+		transform.SetParent (Camera.main.GetComponent<Transform> (), true);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		ScoreText.text = "Score:\n" + GameController.Instance.getScore ();
 	}
+
+
 }
