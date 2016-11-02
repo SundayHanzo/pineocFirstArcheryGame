b2906486a6ba9b8c279de9afa642553f8ef5afd6
Allen Lee <pineoc@naver.com>
2016-11-03 01:21:58 +0900

    make fireRate process

--- Assets/ArrowFire.cs
+++ Assets/ArrowFire.cs
@@ -5,6 +5,8 @@ public class ArrowFire : MonoBehaviour {
 	public GameObject Quad_arrow = null;
 	public Transform ArrowLocation = null;
 	private float powerVal = 2.0f;
+	private const float fireRate = 0.5f;
+	private float nextFire = 0.0f;
 
 	// Use this for initialization
 	void Start () {
@@ -16,18 +18,21 @@ public class ArrowFire : MonoBehaviour {
 	void Update () {
 		if ((Input.touchCount > 0) || Input.GetKey (KeyCode.Space) == true) {
 			//touch down started
-			if(powerVal < 6.0f)
+			if(powerVal < 6.0f && Time.time > nextFire)
 				powerVal += 0.1f;
 		}
 		if ((Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended) || 
 			Input.GetKeyUp(KeyCode.Space) == true) {
 			//touch up ended
-			//Vector3 ArrowFirePos = Camera.main.ScreenToWorldPoint (new Vector3(Screen.width/2, Screen.height/2, Camera.main.nearClipPlane));
-
 			//arrow object set, addforce
-			GameObject arrowObj = Instantiate (Quad_arrow, ArrowLocation.position, ArrowLocation.rotation) as GameObject;
-			arrowObj.GetComponent<Rigidbody> ().AddForce (Camera.main.transform.forward * 500 * powerVal);
-			powerVal = 2.0f;
+			if(Time.time > nextFire){
+				nextFire = Time.time + fireRate;
+				GameObject arrowObj = Instantiate (Quad_arrow, ArrowLocation.position, ArrowLocation.rotation) as GameObject;
+				Vector3 t = Camera.main.transform.forward;
+				t.y += 0.05f;
+				arrowObj.GetComponent<Rigidbody> ().AddForce (t * 500 * powerVal);
+				powerVal = 2.0f;
+			}
 		}
 	}
 }
