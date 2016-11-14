9477cc7cc74ad2b8b201a576952c181c82fd5b7a
Allen Lee <pineoc@naver.com>
2016-11-15 01:41:57 +0900

    add game start scene

---
+++ Assets/MenuSceneScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class MenuSceneScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public void GotoGameScene(){
+		SceneManager.LoadScene ("demoScene");
+	}
+}
